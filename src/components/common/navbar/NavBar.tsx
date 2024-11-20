57aef4653d2c511f9f0d091da3741d3a55cf939b
Toni Pellinen <toni.pellinen@anders.com>
2024-11-20 14:09:53 +0200

    Restore missing navigation menu

--- src/components/common/navbar/NavBar.tsx
+++ src/components/common/navbar/NavBar.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Header, Logo, logoFi, LanguageOption, WithAuthentication } from 'hds-react';
+import { Header, Logo, logoFi, LanguageOption, WithAuthentication, useOidcClient } from 'hds-react';
 import { useTranslation } from 'react-i18next';
 
 import Login from '../auth/Login';
 import Logout from '../auth/Logout';
+import { ROUTES } from '../../../enums';
 
 const T_PATH = 'components.common.navbar.Navbar';
 
 const NavBar = (): JSX.Element => {
   const { t, i18n } = useTranslation();
+  const { isAuthenticated } = useOidcClient();
 
   const languages: LanguageOption[] = [
     { label: 'Suomi', value: 'fi', isPrimary: true },
@@ -33,6 +35,14 @@ const NavBar = (): JSX.Element => {
 
         <WithAuthentication AuthorisedComponent={Logout} UnauthorisedComponent={Login} />
       </Header.ActionBar>
+
+      {isAuthenticated() && (
+        <Header.NavigationMenu>
+          <Header.Link href={`/${ROUTES.PROJECTS}`} label={t(`${T_PATH}.projects`)} />
+          <Header.Link href={`/${ROUTES.CUSTOMERS}`} label={t(`${T_PATH}.customers`)} />
+          <Header.Link href={`/${ROUTES.REPORTS}`} label={t(`${T_PATH}.reports`)} />
+        </Header.NavigationMenu>
+      )}
     </Header>
   );
 };
